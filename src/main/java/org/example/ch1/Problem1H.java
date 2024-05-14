168e67ea2c71eab41a8a486fd64569f6113d029f
minturtle <minseok0425@kakao.com>
2024-05-14 16:08:19 +0900

    Refactor : 도감을 별도의 클래스로 분리 #8

--- src/main/java/org/example/ch1/Problem1H.java
+++ src/main/java/org/example/ch1/Problem1H.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Problem1H {
 
 
-    static String[] dict;
+    static Dict dict;
 
     public static void main(String ... args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,20 +15,21 @@ public class Problem1H {
 
         int[] nm = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        dict = new String[nm[0] + 1];
+        dict = new Dict(nm[0]);
+
 
         // 도감은 1번부터 사용
         for(int i = 1; i <= nm[0]; i++){
-            dict[i] = br.readLine().trim();
+            dict.add(br.readLine().trim(), i);
         }
 
         for(int i = 0; i < nm[1]; i++){
             String question = br.readLine().trim();
             if(isNumber(question)){
-                bw.write(dict[Integer.parseInt(question)]);
+                bw.write(dict.find(Integer.parseInt(question)));
             }
             else{
-                bw.write(getIndex(question));
+                bw.write(dict.find(question));
             }
             bw.newLine();
         }
@@ -41,9 +42,6 @@ public class Problem1H {
     }
 
 
-    /**
-     * O(L) : Input Length L
-     */
 
     static boolean isNumber(String input){
         try{
@@ -55,20 +53,34 @@ public class Problem1H {
     }
 
 
-    /**
-     *  O(N * L)
-     *
-     */
 
-    static String getIndex(String input){
-        for(int i = 1; i < dict.length; i++){
-            if(!dict[i].equals(input)){
-                continue;
-            }
-            return Integer.toString(i);
+
+    static class Dict{
+        private String[] dict;
+
+        public Dict(int size){
+            dict = new String[size + 1];
         }
 
-        return "-1";
+        public void add(String name, int idx){
+            dict[idx] = name;
+        }
+
+        public String find(int idx){
+            return dict[idx];
+        }
+
+        public String find(String name){
+            for(int i = 1; i < dict.length; i++){
+                if(!dict[i].equals(name)){
+                    continue;
+                }
+                return Integer.toString(i);
+            }
+
+            return "-1";
+        }
+
     }
 
 }
