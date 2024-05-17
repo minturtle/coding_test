1d64d303420ebb43ddd559e54fc18c9fbd6a962e
minturtle <minseok0425@kakao.com>
2024-05-17 16:56:12 +0900

    Feat : 좋은 단어 문제 풀이 #13

--- src/main/java/org/example/ch1/Problem1M.java
+++ src/main/java/org/example/ch1/Problem1M.java
@@ -35,44 +35,26 @@ public class Problem1M {
 
 
     public static boolean isGoodWord(String str){
-        /*
-        * Good Word가 아닌 경우
-        * 1. A또는 B의 갯수가 홀수인 경우
-        * 2. 아치안에 문자열이 홀수개 인경우
-        * */
-        int[] archSelected = new int[str.length()];
+
         char[] cArr = str.toCharArray();
 
-        for(int idx = 0; idx < cArr.length; idx++){
-            if(archSelected[idx] == 1){
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < cArr.length; i++){
+            if(stack.isEmpty()){
+                stack.push(cArr[i]);
                 continue;
             }
-
-            int pairIdx = findPair(idx, cArr, archSelected);
-            if(pairIdx == -1){
-                return false; //아치 생성이 불가능하면 갯수가 홀수
+            if(stack.peek() == cArr[i]){
+                stack.pop();
+                continue;
             }
-            if((pairIdx - idx -1) % 2 == 1){
-                return false; // 생성된 아치 사이에 문자의 갯수가 홀수면 안됨.
-            }
-
+            stack.push(cArr[i]);
         }
 
-        return true;
+        return stack.isEmpty();
     }
 
-    public static int findPair(int idx, char[] cArr, int[] archSelected){
-        char c = cArr[idx];
-        for(int i = cArr.length-1; i > idx; i--){
-            if(cArr[i] == c && archSelected[i] == 0){
-                archSelected[idx] = 1;
-                archSelected[i] = 1;
-                return i;
-            }
-        }
-
-        return -1;
-    }
 
 }
 
