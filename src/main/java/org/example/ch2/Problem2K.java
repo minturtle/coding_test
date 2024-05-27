cc4b9594a95bd5f4be329c5880a949db1ecea0d1
minturtle <minseok0425@kakao.com>
2024-05-27 14:43:20 +0900

    Feat : 교수가 된 현우 문제 해결 #25

--- src/main/java/org/example/ch2/Problem2K.java
+++ src/main/java/org/example/ch2/Problem2K.java
@@ -27,50 +27,16 @@ public class Problem2K {
 
 
     static int getRightZeroCount(int number){
-        /*
-        * twoFiveCount[0] 은 팩토리얼에서 2의 갯수
-        * twoFiveCount[1] 은 팩토리얼에서 5의 갯수
-        * */
-        int[] twoFiveCount = new int[2];
 
-        for(int i = 1; i <= number; i++){
-            twoFiveCount[0] += getTwoCount(i);
-            twoFiveCount[1] += getFiveCount(i);
+        int fiveCount = 0;
+
+        for(int i = 1; Math.pow(5, i) <= number; i++){
+            fiveCount += number / Math.pow(5, i);
         }
 
-        int min = Math.min(twoFiveCount[0], twoFiveCount[1]);
 
-        return min;
+        return fiveCount;
 
-    }
-
-
-    // num에서 2로 나누어지는 횟수가 몇 회인지 구하는 함수
-    static int getTwoCount(int num){
-        int cnt = 0;
-        while(true){
-            if(num % 2 != 0){
-                break;
-            }
-            num /= 2;
-            cnt++;
-        }
-
-        return cnt;
-    }
-
-    // num에서 5로 나누어지는 횟수가 몇 회인지 구하는 함수
-    static int getFiveCount(int num){
-        int cnt = 0;
-        while(true){
-            if(num % 5 != 0){
-                break;
-            }
-            num /= 5;
-            cnt++;
-        }
-
-        return cnt;
     }
 
 }
