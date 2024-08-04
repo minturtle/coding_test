cae29befa8be92a0064f507f7864f23a65fa86c9
minturtle <minseok0425@kakao.com>
2024-08-04 14:02:57 +0900

    feat: 2*n 타일링2 문제 해결

---
+++ src/main/java/org/example/ch7/Problem11727.java
@@ -0,0 +1,37 @@
+package org.example.ch7;
+
+import java.io.*;
+import java.util.*;
+
+public class Problem11727 {
+
+    public static void main(String[] args) throws IOException{
+        try(
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ){
+            int n = Integer.parseInt(br.readLine());
+            if(n == 1){
+                bw.write("1");
+                bw.flush();
+            }
+
+            bw.write(Integer.toString(execute(n)));
+
+        }
+    }
+
+    static int execute(int n){
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i-1] + (dp[i -2]) * 2) % 10007;
+        }
+
+        return dp[n];
+    }
+
+
+}
