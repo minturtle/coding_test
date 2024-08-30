5723c91f506978b82a7b7bd8ffd493499cf2e8d2
minturtle <minseok0425@kakao.com>
2024-08-30 11:16:01 +0900

    feat: 서울에서 경산까지 문제 해결 - DP 탑다운 방식 #120

--- src/main/java/org/example/ch7/Problem7V.java
+++ src/main/java/org/example/ch7/Problem7V.java
@@ -9,6 +9,7 @@ public class Problem7V {
     private static int k;
     private static Route[] routes;
 
+    private static int[][] dp;
 
     public static void main(String[] args) throws IOException{
         try(
@@ -20,6 +21,11 @@ public class Problem7V {
             k = Integer.parseInt(st.nextToken());
 
             routes = new Route[n];
+            dp = new int[k+1][n];
+
+            for(int i = 0; i <= k; i++){
+                Arrays.fill(dp[i], -1);
+            }
 
             for(int i = 0; i < n; i++){
                 st = new StringTokenizer(br.readLine());
@@ -33,33 +39,34 @@ public class Problem7V {
             }
 
 
-            bw.write(Integer.toString(dfs(0, 0,0)));
+            bw.write(Integer.toString(dfs(0,0)));
             bw.flush();
         }
     }
 
 
-    private static int dfs(int money, int time, int idx){
+    private static int dfs(int time, int idx){
         if(time > k){
-            return -1;
+            return Integer.MIN_VALUE;
         }
         if(idx >= n){
-            return money;
+            return 0;
+        }
+        if(dp[time][idx] != -1){
+            return dp[time][idx];
         }
         Route route = routes[idx];
 
-        int result = dfs(money + route.walkMoney,time + route.walkTime, idx+1);
-        result = Math.max(
-                dfs(money + route.cycleMoney, time + route.cycleTime, idx+1),
-                result
+
+        dp[time][idx] = Math.max(
+                dfs(time + route.cycleTime, idx+1) + route.cycleMoney,
+                dfs(time + route.walkTime, idx+1) + route.walkMoney
         );
 
-        return result;
+        return dp[time][idx];
     }
 
     private static class Route{
-
-
         public Route(int walkTime, int walkMoney, int cycleTime, int cycleMoney) {
             this.walkTime = walkTime;
             this.walkMoney = walkMoney;
